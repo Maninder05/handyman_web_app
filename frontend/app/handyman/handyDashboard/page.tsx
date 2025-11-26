7656fd3eb60c7502a19868b609052f24e2ed83e4
vijay_chhetri <vijaytchhetri@gmail.com>
2025-11-26 14:21:44 -0700

    Update page.tsx

--- frontend/app/handyman/handyDashboard/page.tsx
+++ frontend/app/handyman/handyDashboard/page.tsx
@@ -73,7 +73,7 @@ export default function HandyDashboard() {
     return () => {
       document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+
   }, []);
 
   const fetchProfile = async () => {
