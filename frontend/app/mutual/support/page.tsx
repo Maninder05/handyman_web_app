9c41980be51c41f0a98192e94d835aba7165a89e
Maninder05 <165261256+Maninder05@users.noreply.github.com>
2025-12-10 13:59:09 -0700

    commit

--- frontend/app/mutual/support/page.tsx
+++ frontend/app/mutual/support/page.tsx
@@ -335,7 +335,7 @@ export default function HelpCentrePage() {
               <path
                 fillRule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clipRule="evenodd"
+                clipRule="evenodd" //comment 
               />
             ) : (
               <path
